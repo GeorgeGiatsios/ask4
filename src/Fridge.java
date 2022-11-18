93c1b2ea3ab48058acb1666b0be556dd1056fb3d
George Giatsios <giwrgos240203@gmail.com>
2022-11-18 14:47:13 +0200

    Initial commit

---
+++ src/Fridge.java
@@ -0,0 +1,74 @@
+public class Fridge {
+    static float mikos = 100;
+    static float vathos = 50;
+    static float ipsos = 150;
+    static String manufacturername = " Inventor";
+    static String devicename = " DPC1430W ";
+    static String devicetype = " Fridge";
+    static String energyconsumption = " 3A+";
+
+    static void setMikos(float x) {
+        mikos = x;
+    }
+    static void setVathos(float y) {
+        mikos = y;
+    }
+    static void setIpsos(float z) {
+        mikos = z;
+    }
+    static void setManufacturername(int a) {
+        manufacturername = String.valueOf(a);
+    }
+    static void setDevicename(int b) {
+        manufacturername = String.valueOf(b);
+    }
+    static void setDevicetype(int c) {
+        manufacturername = String.valueOf(c);
+    }
+    static void setKatanalosiEnergeias(int d) {
+        manufacturername = String.valueOf(d);
+    }
+
+
+    public static void main(String[] args) {
+        if (mikos !=100) {
+            System.out.println("ERROR");
+        } else {
+            System.out.println("To mikos einai:" + mikos);
+        }
+        if (vathos !=50) {
+            System.out.println("ERROR");
+        } else {
+            System.out.println("To vathos einai:" + vathos);
+        }
+        if (ipsos !=150) {
+            System.out.println("ERROR");
+        } else {
+            System.out.println("To ipsos einai:" + ipsos);
+        }
+        if(manufacturername == null){
+            System.out.println("ERROR");
+        }
+        else{
+            System.out.println("To onoma toy kataskevasti einai:" + manufacturername);
+        }
+        if(devicename == null){
+            System.out.println("ERROR");
+        }
+        else{
+            System.out.println("To onoma tis siskeuis einai:" + devicename);
+        }
+        if(devicetype == null){
+            System.out.println("ERROR");
+        }
+        else{
+            System.out.println("O typos tis siskeuis einai:" + devicetype);
+        }
+        if(energyconsumption == null){
+            System.out.println("ERROR");
+        }
+        else{
+            System.out.println("H katanalosi energeias einai:" + energyconsumption );
+        }
+    }
+}
